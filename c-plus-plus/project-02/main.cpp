0f3f1123156d429d0ea85b76a7daa1fb2415fbe2
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2020-02-10 00:27:43 +0200

    added driver code

--- c-plus-plus/project-02/main.cpp
+++ c-plus-plus/project-02/main.cpp
@@ -1,7 +1,36 @@
+#include "car.h"
 #include <iostream>
 
 int main()
 {
-    std::cout<<"Hello, World!"<<std::endl;
+    /**
+     * Create a car object with empty constructor.
+     * Need to use the setter methods to add the car info.
+     */
+
+    Car car_1;
+    car_1.setModel("Volkswagen Polo");
+    car_1.setColor("red");
+    car_1.setMakeYear("2017");
+    car_1.setFuelType("Petrol");
+
+    std::cout << "Just bought a " << car_1.getColor() << " "  << car_1.getModel();
+    std::cout << " " << car_1.getMakeYear() << " that uses " << car_1.getFuelType() << ".\n";
+    car_1.start();
+    car_1.stop();
+
+
+    /**
+     * Create a car object with overloaded constructor.
+     * Dont' need to use the setter methods to add the car info.
+     */
+
+    Car car_2("Volkswagen Vento", "black", "2017", "Diesel");
+
+    std::cout << "\nJust bought a " << car_2.getColor() << " "  << car_2.getModel();
+    std::cout << " " << car_2.getMakeYear() << " that uses " << car_2.getFuelType() << ".\n";
+    car_2.start();
+    car_2.stop();
+
     return 0;
 }
